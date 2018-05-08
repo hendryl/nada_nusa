6e2bd7f2a8e141ae9930e4f20de571809611d37c
Hendry <email.hendry.lee@gmail.com>
2018-05-08 22:57:48 +0700

    Create Base Homepage

---
+++ Assets/Scenes/Main Menu/MainMenuScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+
+public class MainMenuScript : MonoBehaviour {
+	public Button settingsButton, infoButton;
+
+	// Use this for initialization
+	void Start () {
+		Button settings = settingsButton.GetComponent<Button>();
+		Button info = infoButton.GetComponent<Button>();
+
+		settings.onClick.AddListener(OnClickSettings);
+		info.onClick.AddListener(OnClickInfo);
+	}
+
+	void OnClickSettings () {
+		Debug.Log("Settings button clicked");
+	}
+
+	void OnClickInfo () {
+		Debug.Log("Info button clicked");
+	}
+}
